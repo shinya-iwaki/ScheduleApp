b417d74dfb07c90b6896fe08e00fa2bb15f10213
shinya <s-iwaki@clinks.jp>
2020-11-24 11:49:19 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/tamon/flutter_schedule/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.tamon.flutter_schedule
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_schedule/calendar.dart';
+import 'package:flutter_schedule/theme.dart';
+import 'package:provider/provider.dart';
+
+void main() async {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => ThemeNotifier(),
+      child: Consumer<ThemeNotifier>(
+          builder: (context, ThemeNotifier notifier, child) {
+            return MaterialApp(
+              debugShowCheckedModeBanner: false,
+              theme: notifier.isDarkTheme ? dark : light,
+              home: Calendar(),
+            );
+          }),
+    );
+  }
+}
+
